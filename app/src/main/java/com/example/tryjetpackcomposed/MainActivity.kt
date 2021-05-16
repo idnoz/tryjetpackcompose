718a313ed5d6cfad23f514c0efcdc00a93cc69d3
Novan Zunaedi <novan.zunaedi@idntimes.com>
2021-05-16 15:59:06 +0700

    10_Side_effects_and_Effect_Handlers

--- app/src/main/java/com/example/tryjetpackcomposed/MainActivity.kt
+++ app/src/main/java/com/example/tryjetpackcomposed/MainActivity.kt
@@ -5,36 +5,55 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.tryjetpackcomposed.ui.theme.TryJetpackComposedTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Row(
-                modifier = Modifier
-                    .width(WIDTH.dp)
-                    .fillMaxHeight(fraction = HEIGHT)
-                    .background(Color.Green),
-                horizontalArrangement = Arrangement.SpaceEvenly,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(text = "Hello")
-                Text(text = "World")
-                Text(text = "Test")
+
+            val scaffoldState = rememberScaffoldState()
+            val scope = rememberCoroutineScope()
+            Scaffold(scaffoldState = scaffoldState) {
+                var counter = produceState(initialValue = 0) {
+                    delay(3000L)
+                    value = 4
+                }
+                if (counter.value % 5 == 0 && counter.value > 0) {
+                    LaunchedEffect(key1 = scaffoldState.snackbarHostState) {
+                        scaffoldState.snackbarHostState.showSnackbar("Hello")
+                    }
+                }
+                Button(onClick = { }) {
+                    Text(text = "Click Me ${counter.value}")
+                }
             }
+
+//            val scaffoldState = rememberScaffoldState()
+//            val scope = rememberCoroutineScope()
+//            Scaffold(scaffoldState = scaffoldState) {
+//                var counter by remember {
+//                    mutableStateOf(0)
+//                }
+//                if (counter % 5 == 0 && counter > 0) {
+//                    LaunchedEffect(key1 = scaffoldState.snackbarHostState) {
+//                        scaffoldState.snackbarHostState.showSnackbar("Hello")
+//                    }
+//                }
+//                Button(onClick = { counter++ }) {
+//                    Text(text = "Click Me ${counter}")
+//                }
+//            }
         }
     }
 }
-
-const val WIDTH = 300
-const val HEIGHT = 0.7f
